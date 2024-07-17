95aa3f0cc365c54af31f9b65a065aca1648993bd
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-07-17 12:59:53 -0700

    .env, sepoliaCofig added. hardhat.config updated for sepolia deplyment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+const privateKey = process.env.PRIVATE_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    localhost: {},
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [privateKey],
+    },
+  },
 };
